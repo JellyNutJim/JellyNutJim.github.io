3af2690d86e7b68b7380b8adb41e3a6cd621c948
JellyNutJim <oscarhoodsmith@gmail.com>
2021-11-21 17:47:50 +0000

    tried to add animation functinality

--- bohr.js
+++ bohr.js
@@ -35,51 +35,87 @@ function drawCircle(x, y, r, colour = "rgba(255, 255, 255, 0)"){
     c.stroke();
 }
 
-function displayElement2D(Symbol, atmoicNum, atomicWeight){
+//Creates a circle, takes position, radius and colour as parameters.
+function drawAnimCircle(x, y, r, colour = "rgba(255, 255, 255, 0)", time){
+    var canvas = document.getElementById("displayCanvas");
+    var c = canvas.getContext("2d");
+
+    c.fillStyle = colour;
+    c.beginPath();
+    c.arc(x, y, r, 0, 2 * Math.PI);
+    c.rotate(((2 * Math.PI) / 60) * time.getSeconds() + ((2 * Math.PI) / 60000) * time.getMilliseconds());
+    c.fill();
+    c.stroke();
+}
+
+function animCircle(x, y, r, colour){
+    var canvas = document.getElementById("displayCanvas");
+    var c = canvas.getContext("2d");
+
+    this.x = x;
+    this.y = y;
+    this.r = r;
+    this.colour = colour;
+
+    this.draw = function(time){
+        c.beginPath();
+        c.fillStyle = this.colour;
+        c.arc(x, y, r, 0, 2 * Math.PI);
+        c.rotate(((2 * Math.PI) / 60) * time.getSeconds() + 1 + ((2 * Math.PI) / 60000) * time.getMilliseconds());
+        c.fill();
+        c.stroke();
+        console.log(this.y);
+    }
+}
+
+function displayElement2D(elementSymbol, atmoicNum, atomicWeight){
     //Define all needed aspects.
     var protons = atmoicNum;
     var electrons = atmoicNum;
     var neutrons = Math.floor(atomicWeight - atmoicNum);
     var electronStructure = getBohrElectronStructure(electrons)[0]; //E.g. 2.8.8.4
     var electronShells = getBohrElectronStructure(electrons)[1]; //E.g.  4 shells
+    var time = new Date();
 
 
     //Find canvas element and define drawing tool.
     var canvas = document.getElementById("displayCanvas");
     var drawCanvas = canvas.getContext("2d");
+    drawCanvas.clearRect(0, 0, canvas.width, canvas.height);
 
-    //Set width and height variables. Will be used to calculate the mid point of the canvas.
-    var width = canvas.width;
-    var height = canvas.height;
-    var mWidth = width / 2;
-    var mHeight = height / 2;
+    //Translate the point of origin to the centre of the canvas.
+    drawCanvas.save();
+    drawCanvas.translate(canvas.width / 2, canvas.height / 2);
 
     //Create Centre
-    drawCircle(mWidth, mHeight, 30, "red");
+    drawCircle(0, 0, 30, "red");
+    
 
     //create shells and electrons
     for (var i = 0; i < electronShells; i++){
-        console.log(i);
-        drawCanvas.beginPath();
-        drawCanvas.arc(mWidth, mHeight, 80 * (i + 1), 0, 2 * Math.PI);
-        drawCanvas.stroke();
-
+        drawCircle(0, 0, 80 * (i + 1), "rgba(255, 255, 255, 0)");
         
         if (i == 0){
             if (electronStructure[i] > 0){
-                drawCircle(mWidth, mHeight - 80, 15, "yellow");
+                var elec1 = new animCircle(0, -80, 15, "yellow", time);
+                elec1.draw(time);
             }
             if (electronStructure[i] > 1){
-                drawCircle(mWidth, mHeight + 80, 15, "yellow");
-            }
-            
+                var elec2 = new animCircle(0, 80, 15, "yellow", time);
+                elec2.draw(time);
+            } 
         } else{
             for (var g = 0; g < electronStructure[i]; g++){
 
             }
         }
     }
+    
+    drawCanvas.restore();
+    console.log("e");
 }
+
+
 
 //The main entry point for the bohr model functions.
 function bohrMain(userInput){
@@ -92,7 +128,8 @@ function bohrMain(userInput){
     var atmoicNum = 11;
     var atomicWeight = 23;
 
-    window.requestAnimationFrame(displayElement2D(userInput, atmoicNum, atomicWeight));
+    displayElement2D(userInput, atmoicNum, atomicWeight);
+    setInterval(function() { displayElement2D(userInput, atmoicNum, atomicWeight); }, 50);
     return [false, "Run Succesfull"];
 }
 
