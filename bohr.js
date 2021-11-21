aa18c30c460acc9432a68bc855d91de3ecd3a834
JellyNutJim <oscarhoodsmith@gmail.com>
2021-11-21 16:16:19 +0000

    Further improved the bohr model display function

--- bohr.js
+++ bohr.js
@@ -1,3 +1,88 @@
+//Splits the entered amount of electrons into shells.
+function getBohrElectronStructure(electrons){
+    var shells =  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]; //15 possible shells.
+    var counter = 1;
+
+    //As the first shell difers in size to the rest, we define it first.
+    if (electrons > 2){
+        shells[0] =  2;
+        electrons -= 2;
+    }
+
+    //Run through the remaining number of electrons, increasing the electrons in each shell.
+    //Max of 8 electrons per shell.
+    for (var i = 1; i < electrons + 1; i++){
+        if (i % 8 != 0){
+            shells[counter] += 1;
+        } else{
+            shells[counter] += 1;
+            counter++;
+        }
+    }
+    //Returns the electron structure as well as the number of shells.
+    return [shells, counter + 1];
+}
+
+//Creates a circle, takes position, radius and colour as parameters.
+function drawCircle(x, y, r, colour = "rgba(255, 255, 255, 0)"){
+    var canvas = document.getElementById("displayCanvas");
+    var c = canvas.getContext("2d");
+
+    c.fillStyle = colour;
+    c.beginPath();
+    c.arc(x, y, r, 0, 2*Math.PI);
+    c.fill();
+    c.stroke();
+}
+
+function displayElement2D(Symbol, atmoicNum, atomicWeight){
+    //Define all needed aspects.
+    var protons = atmoicNum;
+    var electrons = atmoicNum;
+    var neutrons = Math.floor(atomicWeight - atmoicNum);
+    var electronStructure = getBohrElectronStructure(electrons)[0]; //E.g. 2.8.8.4
+    var electronShells = getBohrElectronStructure(electrons)[1]; //E.g.  4 shells
+
+
+    //Find canvas element and define drawing tool.
+    var canvas = document.getElementById("displayCanvas");
+    var drawCanvas = canvas.getContext("2d");
+
+    //Set width and height variables. Will be used to calculate the mid point of the canvas.
+    var width = canvas.width;
+    var height = canvas.height;
+    var mWidth = width / 2;
+    var mHeight = height / 2;
+
+    //Create Centre
+    drawCircle(mWidth, mHeight, 30, "red");
+
+    //create shells and electrons
+    for (var i = 0; i < electronShells; i++){
+        console.log(i);
+        drawCanvas.beginPath();
+        drawCanvas.arc(mWidth, mHeight, 80 * (i + 1), 0, 2 * Math.PI);
+        drawCanvas.stroke();
+
+        
+        if (i == 0){
+            if (electronStructure[i] > 0){
+                drawCircle(mWidth, mHeight - 80, 15, "yellow");
+                dr
+            }
+            if (electronStructure[i] > 1){
+                drawCircle(mWidth, mHeight + 80, 15, "yellow");
+            }
+            
+        } else{
+            for (var g = 0; g < electronStructure[i]; g++){
+
+            }
+        }
+    }
+}
+
+//The main entry point for the bohr model functions.
 function bohrMain(userInput){
     if(!checkValidElement(userInput)){
         var error = [true, "Invalid Element"]
@@ -8,50 +93,7 @@ function bohrMain(userInput){
     var atmoicNum = 11;
     var atomicWeight = 23;
 
-    displayElement2D(userInput, atmoicNum, atomicWeight);
+    window.requestAnimationFrame(displayElement2D(userInput, atmoicNum, atomicWeight));
     return [false, "Run Succesfull"];
 }
 
-function getBohrElectronStructure(electrons){
-    var shells = [0, 0, 0, 0];
-    var counter = 1;
-    var counter2 = 0;
-
-    if (electrons > 2){
-        shells[0] =  2;
-        electrons -= 2;
-    }
-
-    for (var i = 1; i < electrons + 1; i++){;
-        if (i % 9 != 0){
-            shells[counter] += 1;
-        } 
-        else{
-            counter++;
-        }
-    }
-
-    return shells;
-}
-
-function displayElement2D(Symbol, atmoicNum, atomicWeight){
-    //Define all needed aspects.
-    var protons = atmoicNum;
-    var electrons = atmoicNum;
-    var electronStructure = getBohrElectronStructure(electrons);
-    var neutrons = Math.floor(atomicWeight - atmoicNum);
-
-    console.log(electronStructure);
-
-    //Find canvas element and define drawing tool.
-    var canvas = document.getElementById("displayCanvas");
-    var drawCanvas = canvas.getContext("2d");
-
-    var width = canvas.width;
-    var height = canvas.height;
-
-    //Draw bohr model
-    drawCanvas.fillStyle = "#FF0000";
-    
-
-}
--- display.js
+++ display.js
@@ -1,8 +1,31 @@
+//This function will set the canvas size equal to its parent element ('canvasHolder')
+//Called whenever a change in window size is detected
+window.addEventListener('resize', function(event){
+    let mainCanvas = document.querySelector('.canvasHolder');
+    var canvas = document.getElementById("displayCanvas");
+    var pwidth = mainCanvas.clientWidth;
+    var pheight = mainCanvas.clientHeight;
+
+    canvas.setAttribute('width', pwidth);
+    canvas.setAttribute('height', pheight);
+    initializeDisplay();
+});
+
+//Takes the users inputs, and calls the appropriate functions based on said input.
 function initializeDisplay(){
     //Define user values
     var userInput = document.getElementById("emInput").value;
     var radioOptions = document.getElementsByName("emOption");
     var displayOption;
+
+    //Identical to the resize canvas method, but called once.
+    let mainCanvas = document.querySelector('.canvasHolder');
+    var canvas = document.getElementById("displayCanvas");
+    var pwidth = mainCanvas.clientWidth;
+    var pheight = mainCanvas.clientHeight;
+
+    canvas.setAttribute('width', pwidth);
+    canvas.setAttribute('height', pheight);
 
     //Search radioOptions array for the checked radio, then save the chosen value.
     for (var i = 0; i < radioOptions.length; i++){
@@ -19,6 +42,7 @@ function initializeDisplay(){
             break;
     }
 
+    //temp error displayer
     if (error[0] == true){
         console.log(error[1]);
     }
