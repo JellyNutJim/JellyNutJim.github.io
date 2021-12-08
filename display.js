0d20c93a0d7506e7a85e217b57a68e755730e6e9
JellyNutJim <55330213+JellyNutJim@users.noreply.github.com>
2021-12-08 21:30:13 +0000

    Added a custom mobile display.
    Added a basic mobile webpage that will be detected and launched when needed.

--- display.js
+++ display.js
@@ -1,6 +1,11 @@
 function checkBroswerPlatform(){
     const isMobile = ('ontouchstart' in document.documentElement && navigator.userAgent.match(/Mobile/));
-    console.log(isMobile);
+    if (isMobile != false){
+        console.log("mobile detected");
+        document.location='mobileIndex.html';
+    } else{
+        console.log("computer detected");
+    }
     initializeDisplay();
 }
 
