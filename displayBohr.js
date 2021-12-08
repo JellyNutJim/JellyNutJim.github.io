27ff1152daf34f41fd79d940f5ca5f894135f7b1
JellyNutJim <55330213+JellyNutJim@users.noreply.github.com>
2021-12-08 20:17:58 +0000

    Update displayBohr.js

--- displayBohr.js
+++ displayBohr.js
@@ -162,7 +162,7 @@ function bohrMain(userInput){
 
     //Pre load images into variables
     protonImage.src = 'proton.png';
-    electronImage.src = 'electron.png'
+    electronImage.src = 'Electron.png'
 
     window.requestAnimationFrame(displayElement2D);
     return [false, "Run Succesfull"];
